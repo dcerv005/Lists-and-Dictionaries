f1a11ba4b79fec37b31fd09efee11a99dd9e39a7
dcerv005 <dacervantes12@gmail.com>
2024-08-22 16:28:37 -0700

    creating functions for each task

--- Advanced Operations on Python Lists.py
+++ Advanced Operations on Python Lists.py
@@ -5,12 +5,11 @@ import time
 def squareNumbers (n):
     squaredList = []
     start_time = time.time()
-    for i in range(n):
-        squaredList.append((i+1)**2)
+    squared = [(x+1)**2 for x in range(n)]
     end_time= time.time()
     squaredtime= end_time-start_time
     print(squaredtime)
-    return squaredList
+    return squared
 
     
 #O(n)
@@ -23,22 +22,23 @@ print(squareNumbers(4))
 #Task 2
 
 istVar = ['a','b','c','d']
-start_time= time.time()
-sublist= istVar[1:3]
-r=sublist[::-1]
-end_time= time.time()
-reversedtime= end_time-start_time
-print(reversedtime)
-print(r)
+def reverse(list):
+    start_time= time.time()
+    sublist= list[1:3]
+    r=sublist[::-1]
+    end_time= time.time()
+    reversedtime= end_time-start_time
+    print(reversedtime)
+    print(r)
 
-
+reverse(istVar)
 #Task 3
-
 list1=['Hello', 'World', 'Alphabet']
-
 list2=[1, 2, 3]
+def sortlist(list1, list2):
+    list1.sort()
+    list2.sort()
+    list1.extend(list2)
+    print(list1)
 
-list1.sort()
-list2.sort()
-list1.extend(list2)
-print(list1)
+sortlist(list1, list2)
--- Dictionary Manipulation and Optimization.py
+++ Dictionary Manipulation and Optimization.py
@@ -1,39 +1,63 @@
+import time
+
 #Task1
 
-dict1= {'a': 1, 'b': 2}
-dict2={'c':3, 'd': 4}
 
-dict1.update(dict2)
+def merge_dict(dict1, dict2):
+    start_time= time.time()
+    dict1.update(dict2)
+    end_time= time.time()
 
-print(dict1)
+    total_time=end_time-start_time
+    print("Time to run", total_time)
+    print(dict1)
+
 
 #Task 2
 
-dict3= {"Fantasy": "Harry potter", "Comedy": "Tropic Thunder", "Adventure": "Zathura" }
-dict4= {"Sci-Fi": "Star Wars", "Comedy": "Scary Movie", "Adventure": "Mr. Deeds"}
 
-mergedDict={}
+def merge_dict_intercept(dict1, dict2):
+    start_time = time.time()
+    mergedDict={}
 
-for i in dict3:
-    if i in dict4:
-        mergedDict[i]=[]
-        mergedDict[i].append(dict3[i])
-        mergedDict[i].append(dict4[i])
-
-print(mergedDict)
+    for i in dict3:
+        if i in dict4:
+            mergedDict[i]=[]
+            mergedDict[i].append(dict3[i])
+            mergedDict[i].append(dict4[i])
+    end_time= time.time()
+    total_time=end_time-start_time
+    print("Time to run", total_time)
+    print(mergedDict)
 
 
 
 #Task 3
 
+
+def count_frequency(list):
+    start_time = time.time()
+    dict5={}
+
+    for i in list:
+        if i in dict5:
+            dict5[i] += 1
+        else:
+            dict5[i]=1
+    end_time = time.time()
+    total_time=end_time-start_time
+    print("Time to run", total_time)
+    print(dict5)
+
+
+dict1= {'a': 1, 'b': 2}
+dict2={'c':3, 'd': 4}
+
+dict3= {"Fantasy": "Harry potter", "Comedy": "Tropic Thunder", "Adventure": "Zathura" }
+dict4= {"Sci-Fi": "Star Wars", "Comedy": "Scary Movie", "Adventure": "Mr. Deeds"}
+
 list = [1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 2, 4, 5, 3, 3, 4, 1, 6, 8, 9, 8, 8, 7, 0, 9, 8]
 
-dict5={}
-
-for i in list:
-    if i in dict5:
-        dict5[i] += 1
-    else:
-        dict5[i]=1
-
-print(dict5)
+merge_dict_intercept(dict3, dict4)
+merge_dict(dict1, dict2)
+count_frequency(list)
